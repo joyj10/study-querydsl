3ac8729dcc047d05971c359314635da3f4093c1a
dev_one <micool1030@gmail.com>
2023-09-11 12:12:01 +0900

    upload project

---
+++ src/main/java/study/querdsl/entity/Hello.java
@@ -0,0 +1,24 @@
+package study.querdsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Hello
+ * <pre>
+ * Describe here
+ * </pre>
+ *
+ * @version 1.0,
+ */
+
+@Entity
+@Getter @Setter
+public class Hello {
+    @Id @GeneratedValue
+    private Long id;
+}
