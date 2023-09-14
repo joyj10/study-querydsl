d50e5c9943650f4dc04bb4a8a55f8985eae33b41
dev_one <micool1030@gmail.com>
2023-09-14 22:18:14 +0900

    # 결과 조회 테스트

--- src/test/java/study/querdsl/QuerydslBasicTest.java
+++ src/test/java/study/querdsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querdsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querdsl.entity.Member;
+import study.querdsl.entity.QMember;
 import study.querdsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static study.querdsl.entity.QMember.member;
 
 /**
@@ -101,5 +106,51 @@ class QuerydslBasicTest {
 
         assert findMember != null;
         assertEquals("member", findMember.getUsername());
+    }
+
+    @Test
+    void resultFetch() {
+        // List 조회
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        System.out.println("### fetch = " + fetch);
+
+        // 단건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        System.out.println("### fetchOne = " + fetchOne);
+
+
+        // 처음 한건 조회
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        System.out.println("### fetchFirst = " + fetchFirst);
+
+        // 페이징에서 사용 > * 현재 fetchResults (Deprecated) > count 쿼리 직접 조회로 변경 필요
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        long total = results.getTotal();
+        System.out.println("### total = " + total);
+        List<Member> content = results.getResults();
+        assertNotNull(content);
+
+        // count 쿼리로 변경 > * (Deprecated) > count 쿼리 직접 조회로 변경 필요
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+        assertEquals(4L, count);
+
+        // 개선 된 count query
+        Long count2 = queryFactory
+                .select(member.count())
+                .from(member)
+                .fetchOne();
+        assertEquals(4L, count2);
     }
 }
