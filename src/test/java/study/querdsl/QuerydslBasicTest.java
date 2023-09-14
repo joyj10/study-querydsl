a81dce1feb0f74d98f890b19ed9927032792ead6
dev_one <micool1030@gmail.com>
2023-09-14 22:26:32 +0900

    # 정렬 테스트

--- src/test/java/study/querdsl/QuerydslBasicTest.java
+++ src/test/java/study/querdsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querdsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,7 @@ import javax.persistence.EntityManager;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static study.querdsl.entity.QMember.member;
 
 /**
@@ -152,5 +152,32 @@ class QuerydslBasicTest {
                 .from(member)
                 .fetchOne();
         assertEquals(4L, count2);
+    }
+
+    /*
+      회원 정렬 순서
+      1. 회원 나이 내림 차순(desc)
+      2. 회원 이름 올림 차순(asc)
+      단 2에서 회원 이름이 없으면 마지막에 출력 (nulls last)
+     */
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertEquals("member5", member5.getUsername());
+        assertEquals("member6", member6.getUsername());
+        assertNull(memberNull.getUsername());
     }
 }
